79973f571fc5837289de3df7188379d92601a7be
Lajilit <lajilit@gmail.com>
2021-10-27 23:11:50 +0300

    изменение модели пользователя и миграции

--- todo/users/models.py
+++ todo/users/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(_('email address'), unique=True)
+
+    def __str__(self):
+        return self.email
---
+++ todo/users/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework.serializers import HyperlinkedModelSerializer
+from .models import CustomUser
+
+
+class CustomUserModelSerializer(HyperlinkedModelSerializer):
+   class Meta:
+       model = CustomUser
+       fields = [
+           'firstname',
+           'lastname',
+           'email'
+           ]
