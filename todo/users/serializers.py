36ab0a848b98fc663e88524f7fa103aa491f743a
Lajilit <lajilit@gmail.com>
2021-10-31 21:50:15 +0300

    создано и настроено приложение frontend. Создан компонент User.js настроена работа с бэкендом

--- todo/users/serializers.py
+++ todo/users/serializers.py
@@ -1,13 +1,14 @@
 from rest_framework.serializers import HyperlinkedModelSerializer
+
 from .models import CustomUser
 
 
 class CustomUserModelSerializer(HyperlinkedModelSerializer):
-   class Meta:
-       model = CustomUser
-       fields = [
-           'username',
-           'first_name',
-           'last_name',
-           'email'
-           ]
+    class Meta:
+        model = CustomUser
+        fields = [
+            'username',
+            'first_name',
+            'last_name',
+            'email'
+        ]
