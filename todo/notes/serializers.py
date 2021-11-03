1c7ba8f082fac2d73581b539e6b808a8bc17486a
Lajilit <lajilit@gmail.com>
2021-11-03 23:40:29 +0300

    исправлены serializers

--- todo/notes/serializers.py
+++ todo/notes/serializers.py
@@ -1,19 +1,21 @@
 from rest_framework.relations import StringRelatedField
-from rest_framework.serializers import HyperlinkedModelSerializer
+from rest_framework.serializers import HyperlinkedModelSerializer, \
+    ModelSerializer
 
+from users.models import CustomUser
 from users.serializers import CustomUserModelSerializer
 from .models import Project, Note
 
 
 class ProjectModelSerializer(HyperlinkedModelSerializer):
-    users = StringRelatedField(many=True)
+
     class Meta:
-       model = Project
-       fields = '__all__'
+        model = Project
+        fields = '__all__'
 
 
 class NoteModelSerializer(HyperlinkedModelSerializer):
-   author = CustomUserModelSerializer()
-   class Meta:
-       model = Note
-       fields = '__all__'
+
+    class Meta:
+        model = Note
+        fields = '__all__'
