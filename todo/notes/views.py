da7faebe4b0255700582c6dfc18a25a4f1733677
Lajilit <lajilit@gmail.com>
2021-11-08 20:13:10 +0300

    настроена пагинация

--- todo/notes/views.py
+++ todo/notes/views.py
@@ -1,17 +1,28 @@
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Project, Note
 from .serializers import ProjectModelSerializer, NoteModelSerializer
 
 
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
+
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    pagination_class = ProjectLimitOffsetPagination
+
+
+class NoteLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 20
 
 
 class NoteModelViewSet(ModelViewSet):
     queryset = Note.objects.all()
     serializer_class = NoteModelSerializer
+    pagination_class = NoteLimitOffsetPagination
 
     def destroy(self, request, *args, **kwargs):
         note = self.get_object()  # deleting note
