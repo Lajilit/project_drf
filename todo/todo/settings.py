a81b6ffa2c05dc615abaf4e289b27509f83d8c73
lajil <lajilit@gmail.com>
2021-11-14 20:03:30 +0300

    Добавлена авторизация по JWT

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'django_filters',
     'rest_framework.authtoken',
+    'rest_framework_simplejwt',
 
     'users',
     'notes',
@@ -165,6 +166,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 
 }
