bdfde61bf7cab45f3e18848d62fe9221c3832207
lajil <lajilit@gmail.com>
2021-12-02 00:24:24 +0300

    Настройка документации api

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -15,12 +15,28 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from rest_framework import permissions
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from notes.views import ProjectModelViewSet, NoteModelViewSet
 from users.views import CustomUserCustomViewSet
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="TodoProject",
+      default_version='0.1',
+      description="Documentation to out project",
+      contact=openapi.Contact(email="lajilit@gmail.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny, ),
+)
 
 router = DefaultRouter()
 router.register('users', CustomUserCustomViewSet)
@@ -35,4 +51,7 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include(router.urls)),
     re_path(r'^api/(?P<version>\d\.\d)/', include(router.urls)),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
