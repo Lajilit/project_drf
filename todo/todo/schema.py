ce7518e88c0cdb8440cfc4f2b2fdc47f58e03e75
lajil <lajilit@gmail.com>
2021-12-09 23:48:08 +0300

    Создана мутация для названия проекта

--- todo/todo/schema.py
+++ todo/todo/schema.py
@@ -36,14 +36,14 @@ class Query(graphene.ObjectType):
     all_notes = graphene.List(NoteType)
     all_users = graphene.List(UserType)
 
-    project_by_id = graphene.Field(ProjectType, pk=graphene.Int(required=True))
-    user_by_id = graphene.Field(UserType, pk=graphene.Int(required=True))
+    project_by_id = graphene.Field(ProjectType, id=graphene.Int(required=True))
+    user_by_id = graphene.Field(UserType, id=graphene.Int(required=True))
 
-    project_users = graphene.List(UserType, pk=graphene.Int(required=True))
-    project_notes = graphene.List(NoteType, pk=graphene.Int(required=True))
+    project_users = graphene.List(UserType, id=graphene.Int(required=True))
+    project_notes = graphene.List(NoteType, id=graphene.Int(required=True))
 
-    user_projects = graphene.List(ProjectType, pk=graphene.Int(required=True))
-    user_notes = graphene.List(NoteType, pk=graphene.Int(required=True))
+    user_projects = graphene.List(ProjectType, id=graphene.Int(required=True))
+    user_notes = graphene.List(NoteType, id=graphene.Int(required=True))
 
     def resolve_all_projects(root, info):
         return Project.objects.all()
@@ -54,29 +54,48 @@ class Query(graphene.ObjectType):
     def resolve_all_users(root, info):
         return CustomUser.objects.all()
 
-    def resolve_project_by_id(root, info, pk):
+    def resolve_project_by_id(root, info, id):
         try:
-            return Project.objects.get(pk=pk)
+            return Project.objects.get(id=id)
         except Project.DoesNotExist:
             return None
 
-    def resolve_user_by_id(root, info, pk):
+    def resolve_user_by_id(root, info, id):
         try:
-            return CustomUser.objects.get(pk=pk)
+            return CustomUser.objects.get(id=id)
         except CustomUser.DoesNotExist:
             return None
 
-    def resolve_project_users(root, info, pk):
-        return CustomUser.objects.filter(project__id=pk)
+    def resolve_project_users(root, info, id):
+        return CustomUser.objects.filter(project__id=id)
 
-    def resolve_project_notes(root, info, pk):
-        return Note.objects.filter(project__id=pk)
+    def resolve_project_notes(root, info, id):
+        return Note.objects.filter(project__id=id)
 
-    def resolve_user_projects(root, info, pk):
-        return Project.objects.filter(users__id=pk)
+    def resolve_user_projects(root, info, id):
+        return Project.objects.filter(users__id=id)
 
-    def resolve_user_notes(root, info, pk):
-        return Note.objects.filter(user__id=pk)
+    def resolve_user_notes(root, info, id):
+        return Note.objects.filter(user__id=id)
 
 
-schema = graphene.Schema(query=Query)
+class ProjectNameMutation(graphene.Mutation):
+    class Arguments:
+        name = graphene.String(required=True)
+        id = graphene.ID()
+
+    project = graphene.Field(ProjectType)
+
+    @classmethod
+    def mutate(cls, root, info, name, id):
+        project = Project.objects.get(id=id)
+        project.name = name
+        project.save()
+        return ProjectNameMutation(project=project)
+
+
+class Mutation(graphene.ObjectType):
+    update_project_name = ProjectNameMutation.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
