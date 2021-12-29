ca7ac65663a588d27da1e065ff0dc5116d18c1b0
lajil <lajilit@gmail.com>
2021-12-30 01:22:12 +0300

    создание Docker-контейнера для образов БД и бэкенда

--- production/todo/todo/settings.py
+++ production/todo/todo/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=lwv(mg)m39=8k8^5oz+bzozu#bf@%$8lg#k_9t1!xsz%q@*fl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -86,10 +86,20 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo',
+        'USER': 'lajil',
+        'PASSWORD': '12345',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
--- production/todo/users/management/commands/create_users.py
+++ production/todo/users/management/commands/create_users.py
@@ -1,18 +1,25 @@
 from django.core.management.base import BaseCommand
+
+from notes.models import Note, Project
 from users.models import CustomUser
 
 
 class Command(BaseCommand):
     """ Команда создает новых пользователей в базе данных."""
 
-    def add_arguments(self, parser):
-        parser.add_argument('count', type=int, help="users count")
-
     def handle(self, *args, **options):
-        num = options['count']
-        first = CustomUser.objects.all().count() + 1
+        Note.objects.all().delete()
+        Project.objects.all().delete()
+        CustomUser.objects.all().delete()
+        super_user = CustomUser.objects.create_superuser('lajil',
+                                                         'lajilit@gmail.com',
+                                                         '12345',
+                                                         first_name='Ekaterina',
+                                                         last_name='Vakhrusheva')
+        print(f'Суперпользователь создан: {super_user.username}')
+        first = 1
 
-        for i in range(num):
+        for i in range(10):
             new_user = CustomUser.objects.create_user(
                 f'test_user_{first}',
                 f'test_email_{first}@mail.ru',
